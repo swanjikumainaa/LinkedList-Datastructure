451d37944db358b1030629ce89c15a68e70d9231
swanjikumainaa <swanjikumainaa@gmail.com>
2023-07-12 18:45:01 +0300

    added more methods

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
 
 fun main() {
     var head = createLinkedList()
+    insertAtPosition(4,Node("Mary"),head)
     traverseList(head)
+    println("---------")
+     head = insertAtHead(Node("Joan"),head)
+    traverseList(head)
+    println("---------")
+    removeAtPosition(7,head)
+    traverseList(head)
+    println("---------")
+    insertAfterNode("Joy",Node("Mercy"),head)
+    traverseList(head)
+
+
 
 }
 class Node (var name:String){
@@ -14,7 +26,7 @@ fun createLinkedList(): Node {
     val node4 = Node("Joy")
     val node5 = Node("Valary")
     val node6 = Node("Elizabeth")
-    val node7 = Node("Joyeuese")
+    val node7 = Node("Joyuese")
 
     node1.next=node2
     node2.next=node3
@@ -34,3 +46,43 @@ fun traverseList(head:Node){
     }
 
 }
+
+fun insertAtPosition(pos:Int,nodeToInsert:Node,head:Node){
+    var counter = 1
+    var current:Node? = head
+    while (counter<pos-1){
+        current = current?.next
+        counter++
+    }
+    nodeToInsert.next =current?.next
+    current?.next = nodeToInsert
+}
+
+fun insertAfterNode(name: String,nodeToInsert: Node,head: Node){
+    var current:Node?= head
+    while (current!!.name!=name){
+        current = current.next!!
+    }
+    nodeToInsert.next= current.next
+    current.next = nodeToInsert
+
+
+}
+
+fun insertAtHead(nodeToInsert: Node,head: Node):Node{
+    nodeToInsert.next=head
+
+    return nodeToInsert
+}
+
+fun removeAtPosition(pos:Int,head:Node){
+    var counter = 1
+    var current:Node? = head
+
+    while (counter<pos-1){
+        current = current?.next
+        counter ++
+    }
+    current?.next = current?.next?.next
+}
+
